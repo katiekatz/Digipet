de2c36e21272bb1359bd92938ec0561c91d38c30
Katie <katkatz@bergen.org>
2017-11-17 08:51:34 -0500

    katie

--- Digipet/Scenes/LearnPage.swift
+++ Digipet/Scenes/LearnPage.swift
@@ -11,30 +11,19 @@ import GameplayKit
 import UIKit
 
 class LearnPage: SKScene {
-    var scrollView: UIScrollView!
-    var imageView: UIImageView!
     
     var backArrow: SKSpriteNode!
-    let homePage = SKScene(fileNamed: "GameScene")
-    
+//    let gameScene = SKScene(fileNamed: "GameScene")
     
     override func didMove(to view: SKView) {
+
         //back arrow
         backArrow = childNode(withName: "//backArrow") as! SKSpriteNode
-        //imageView = UIImageView(image: UIImage(named: "image.png"))
-        
-//        scrollView = UIScrollView(frame: view.bounds)
-//        scrollView.backgroundColor = UIColor.red
-//        //scrollView.contentSize = imageView.bounds.size
-//        scrollView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleWidth.rawValue) | UInt8(UIViewAutoresizing.flexibleHeight.rawValue)))
-//        
-//        scrollView.setContentOffset(CGPoint(x: 100, y: -50), animated: true)
-//        //scrollView.addSubview(imageView)
-//        view.addSubview(scrollView)
+
     }
     
     func goToScene(scene: SKScene) {
-        let sceneTransition = SKTransition.fade(with: UIColor.darkGray, duration: 1)
+        let sceneTransition = SKTransition.push(with: .right, duration: 0.5)
         scene.scaleMode = .aspectFill
         self.view?.presentScene(scene, transition: sceneTransition)
     }
@@ -45,9 +34,8 @@ class LearnPage: SKScene {
         let touchLocation = touch.location(in: self)
         
         if backArrow.contains(touchLocation) {
-            let transition:SKTransition = SKTransition.fade(with: UIColor.darkGray, duration: 1)
-            let scene:SKScene = homePage!
-            self.view?.presentScene(scene, transition: transition)
+            let scene:SKScene = SKScene(fileNamed: "GameScene")!
+            goToScene(scene: scene)
         }
         
     }
