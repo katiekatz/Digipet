563568550bb5aa8aebfe1a898def26c9849ebd50
Katie <katkatz@bergen.org>
2017-11-21 08:58:07 -0500

    level 1

--- Digipet/Scenes/LearnPage.swift
+++ Digipet/Scenes/LearnPage.swift
@@ -14,18 +14,19 @@ class LearnPage: SKScene {
     
     var backArrow: SKSpriteNode!
     var menuIcon: SKSpriteNode!
+    var oneTouch: SKSpriteNode!
     
     override func didMove(to view: SKView) {
 
         backArrow = childNode(withName: "//backArrow") as! SKSpriteNode
         menuIcon = childNode(withName: "//menuIcon") as! SKSpriteNode
+        oneTouch = childNode(withName: "//oneTouch") as! SKSpriteNode
         backArrow.position = CGPoint(x: 92, y: 1267)
         menuIcon.position = CGPoint(x: 658, y: 1267)
-
     }
     
-    func goToScene(scene: SKScene) {
-        let sceneTransition = SKTransition.push(with: .right, duration: 0.5)
+    func pushToScene(scene: SKScene, direction: SKTransitionDirection) {
+        let sceneTransition = SKTransition.push(with: direction, duration: 0.5)
         scene.scaleMode = .aspectFill
         self.view?.presentScene(scene, transition: sceneTransition)
     }
@@ -37,7 +38,12 @@ class LearnPage: SKScene {
         
         if backArrow.contains(touchLocation) {
             let scene:SKScene = SKScene(fileNamed: "GameScene")!
-            goToScene(scene: scene)
+            pushToScene(scene: scene, direction: .right)
+        }
+        
+        if oneTouch.contains(touchLocation) {
+            let scene:SKScene = SKScene(fileNamed: "LevelOne")!
+            pushToScene(scene: scene, direction: .left)
         }
         
     }
--- Digipet/Scenes/LevelOne.swift
+++ Digipet/Scenes/LevelOne.swift
@@ -6,4 +6,40 @@
 //  Copyright © 2017 Katie Katz. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+import GameplayKit
+import UIKit
+
+class LevelOne: SKScene {
+    
+    var backArrow: SKSpriteNode!
+    var menuIcon: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        
+        backArrow = childNode(withName: "//backArrow") as! SKSpriteNode
+        menuIcon = childNode(withName: "//menuIcon") as! SKSpriteNode
+        backArrow.position = CGPoint(x: 92, y: 1267)
+        menuIcon.position = CGPoint(x: 658, y: 1267)
+        
+    }
+    
+    func goToScene(scene: SKScene) {
+        let sceneTransition = SKTransition.push(with: .right, duration: 0.5)
+        scene.scaleMode = .aspectFill
+        self.view?.presentScene(scene, transition: sceneTransition)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let touchLocation = touch.location(in: self)
+        
+        if backArrow.contains(touchLocation) {
+            let scene:SKScene = SKScene(fileNamed: "GameScene")!
+            goToScene(scene: scene)
+        }
+        
+    }
+    
+}
